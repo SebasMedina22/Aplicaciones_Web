6658ff72061d2878a5ca76a49280d562e489b4e4
sebas <sebastianmedina063@gmail.com>
2023-09-16 20:23:27 -0500

    Logica SalaryEmployee

--- EmployeeOOP/EmployeeOOP/Classes/SalaryEmployee.cs
+++ EmployeeOOP/EmployeeOOP/Classes/SalaryEmployee.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EmployeeOOP.Classes
+﻿namespace EmployeeOOP.Classes
 {
-    internal class Class1
+    public class SalaryEmployee : Employee
     {
+
+        #region Properties
+        public decimal Salary { get; set; } //Decimal es para trabajar con dinero.
+        #endregion
+
+        #region Methods
+
+        #endregion
+        public override decimal GetValueToPay()
+        {
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" + //base es la clase base o la clase padre
+                $"Salario Devengado: {Salary}\n\t";
+        }
     }
 }
