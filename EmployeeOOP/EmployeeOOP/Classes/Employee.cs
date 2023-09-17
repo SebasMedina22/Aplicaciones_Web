80acb722daae3d1fd8dcd9e25b0f24df16763e16
sebas <sebastianmedina063@gmail.com>
2023-09-16 20:21:53 -0500

    New class SalaryEmployee creado

--- EmployeeOOP/EmployeeOOP/Classes/Employee.cs
+++ EmployeeOOP/EmployeeOOP/Classes/Employee.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace EmployeeOOP.Classes
 {
-    internal class Class1
+    public abstract class Employee //Al menos un método abstracto
     {
+        #region Properties
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public Date? Birthday { get; set; }
+        public Date? HiringDate { get; set; }
+        public bool? IsActive { get; set; }
+        #endregion
+
+        #region Methods
+        public abstract decimal GetValueToPay();
+        public override string ToString()
+        {
+            return $"INFORMACIÓN EMPLEADO \n\t " +
+                $"ID: {Id}\n\t" +
+                $"Nombres: {FirstName}\n\t" +
+                $"Apellidos: {LastName}\n\t" +
+                $"Fecha de cumpleaños: {Birthday}\n\t" +
+                $"Fecha de contratación: {HiringDate}\n\t" +
+                $"¿Empleado activo?: {IsActive}\n\t";
+        }
+
+        #endregion
+
     }
 }
